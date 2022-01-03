c4e6764ca655c72db478007baf12897aaf8d321e
Asude Şenözlüler <senozlulerasude@gmail.com>
2022-01-03 14:14:51 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        char harf='A';
+        switch (harf){
+            case 'A':
+            case 'I':
+            case 'O':
+            case 'U':
+                System.out.println("Kalın Sesli Harf");
+                break;
+            default:
+                System.out.println("İnce Sesli Harf");
+        }
+    }
+}
